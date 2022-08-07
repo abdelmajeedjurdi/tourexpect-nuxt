d2df8bb05dda086e0ae533d0c4d9ef9863cb8851
EidoKh <eido.khudyda@gmail.com>
2022-08-07 14:32:12 +0300

    mega menu

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -59,9 +59,68 @@
 						</li>
 
 						<li class="w-full md:w-auto text-center">
-							<router-link to="/destinations"
+							<!-- <router-link to="/transfer"
 								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
-								{{ $t('destinations') }}</router-link>
+								{{ $t('transfer') }}</router-link> -->
+							<div>
+								<button
+									class="peer py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">{{
+											$t('destinations')
+									}}</button>
+
+								<!-- the menu here -->
+								<div class="hidden absolute peer-hover:flex hover:flex
+         
+        							 flex-col bg-white drop-shadow-lg">
+
+									<div class="py-4 rounded-md   mx-auto shadow-lg" style="width:67rem ;">
+										<div class="flex  ">
+											<div class="  rounded-md w-2/5">
+												<ul>
+													<li class=" px-4 border-b py-2 hover:bg-orange-500 hover:text-white cursor-pointer"
+														v-for="i in 6"><b>
+															Destination{{ ' ' + i
+															}} </b>
+														<div class="mt-2 text-sm text-gray-400">
+															<span class="mx-2">Baghdad</span>
+															<span class="mx-2">Basrah</span>
+														</div>
+													</li>
+												</ul>
+											</div>
+
+
+
+											<div class=" w-full mx-4">
+												<div class="font-bold mb-4 text-gray-800">Select a country or a city to
+													explore</div>
+												<div>
+													<ul>
+														<li>
+															<div class="grid grid-cols-4 mb-6">
+																<b class="col-span-1"><a href="#">Turkey</a></b>
+																<div class="col-span-3 font-bold text-gray-600"><a
+																		href="#" v-for="j in 7" class="mx-4">{{ 'Town '
+																				+ j
+																		}}</a></div>
+															</div>
+															<div class="grid grid-cols-4">
+																<b class="col-span-1"><a href="#">Turkey</a></b>
+																<div class="col-span-3 font-bold text-gray-600"><a
+																		href="#" v-for="j in 6" class="mx-4">{{ 'Town '
+																				+ j
+																		}}</a></div>
+															</div>
+
+														</li>
+														<li></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</li>
 						<li class="w-full md:w-auto text-center">
 							<router-link to="/packages"
@@ -99,6 +158,7 @@
 								</div>
 							</div>
 						</li>
+
 						<li class="w-full md:w-auto text-center">
 							<router-link to="/test"
 								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
