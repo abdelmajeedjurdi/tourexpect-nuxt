f46285ed0550c62ee272ab48364957d770b87cb0
EidoKh <eido.khudyda@gmail.com>
2022-08-14 13:08:03 +0300

    rrrede

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
             postcssOptions: require("./postcss.config.js"),
         },
         rollupOptions: {
-            external: ['Swiper'],
+            external: ['plugins/swiper.ts'],
         }
     },
 })
