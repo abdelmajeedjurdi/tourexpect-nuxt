b35978ecd0a24f5b42317d73b09ec64d1cfa6399
EidoKh <eido.khudyda@gmail.com>
2022-08-14 13:07:01 +0300

    ddfdsadsa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
             postcssOptions: require("./postcss.config.js"),
         },
         rollupOptions: {
-            external: ['swiper'],
+            external: ['Swiper'],
         }
     },
 })
