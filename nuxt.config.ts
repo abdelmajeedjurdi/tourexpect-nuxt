1a1f3a2a7d921f1f361100afe962f9c53917a7dc
EidoKh <eido.khudyda@gmail.com>
2022-08-14 13:05:53 +0300

    check

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,8 @@ export default defineNuxtConfig({
         postcss: {
             postcssOptions: require("./postcss.config.js"),
         },
+        rollupOptions: {
+            external: ['swiper'],
+        }
     },
 })
