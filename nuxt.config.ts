9fb53b1e0f8c36061df748b8bfa137d3db148c20
EidoKh <eido.khudyda@gmail.com>
2023-02-22 13:10:34 +0300

    swiper

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,32 @@
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    
-      modules: [
-        '@nuxtjs/i18n',
-         '@nuxtjs/tailwindcss'
-      ],
-
-      i18n: {
-        detectBrowserLanguage: true, 
-      defaultLocale: process.env.DEFAULT_LOCALE ?? 'en',
-      strategy: 'prefix_except_default',
-      locales: [
-        'en',
-        'ar'
-      ],
-      vueI18n: {
-        fallbackLocale: process.env.DEFAULT_LOCALE ?? 'en',
-        messages: {
-          'en': require('./translations/en'),
-          'ar': require('./translations/ar')
-        }
-      }
+  modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "nuxt-swiper"],
+  swiper: {
+    // Swiper options
+    //----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
   },
-      
-    runtimeConfig: {
-      public: {
-        API_BASE_URL: process.env.API_BASE_URL,
-        BASE_URL: process.env.BASE_URL
-      }
+
+  i18n: {
+    detectBrowserLanguage: true,
+    defaultLocale: process.env.DEFAULT_LOCALE ?? "en",
+    strategy: "prefix_except_default",
+    locales: ["en", "ar"],
+    vueI18n: {
+      fallbackLocale: process.env.DEFAULT_LOCALE ?? "en",
+      messages: {
+        en: require("./translations/en"),
+        ar: require("./translations/ar"),
+      },
     },
-    
-})
+  },
+
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+      BASE_URL: process.env.BASE_URL,
+    },
+  },
+});
