4c29f1a1ca22ebddd337bb63d5cf78982820e424
EidoKh <eido.khudyda@gmail.com>
2023-02-22 15:15:16 +0300

    done

--- pages/index.vue
+++ pages/index.vue
@@ -136,51 +136,28 @@
         </div>
       </div>
       <div>
-        <!-- <item-slider
+        <item-slider
           :title="$t('best_of_trabzon_tours')"
           :items="province_tours['trabzon']"
-        /> -->
+        />
       </div>
       <div>
-        <!-- <item-slider
+        <item-slider
           :title="$t('best_of_rize_tours')"
           :items="province_tours['rize']"
-        /> -->
+        />
       </div>
       <div>
-        <!-- <item-slider
+        <item-slider
           :title="$t('best_of_east_black_sea_region_tours')"
           :items="province_tours['east-black-sea-region']"
-        /> -->
+        />
       </div>
       <div>
-        <!-- <item-slider
+        <item-slider
           :title="$t('best_of_dubai_tours')"
           :items="province_tours['dubai']"
-        /> -->
-        <Swiper
-          :modules="[SwiperAutoplay, SwiperEffectCreative]"
-          :slides-per-view="1"
-          :loop="true"
-          :effect="'creative'"
-          :autoplay="{
-            delay: 8000,
-            disableOnInteraction: true,
-          }"
-          :creative-effect="{
-            prev: {
-              shadow: false,
-              translate: ['-20%', 0, -1],
-            },
-            next: {
-              translate: ['100%', 0, 0],
-            },
-          }"
-        >
-          <SwiperSlide v-for="slide in 10" :key="slide">
-            <strong>{{ slide }}</strong>
-          </SwiperSlide>
-        </Swiper>
+        />
       </div>
     </div>
   </div>
