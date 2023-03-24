95ecfdb189e8805fd328a65a2caadd4d8401c301
Edo Khudeeda <edo.khudeeda@gmail.com>
2023-03-24 22:04:51 +0300

    1

--- pages/visas/[country]/[visa]/payment.vue
+++ pages/visas/[country]/[visa]/payment.vue
@@ -209,7 +209,7 @@ const applyToVisa = async (form) => {
 
   try {
     let { data: application } = await useFetch(() => `visa-application`, {
-      baseURL: /*config.API_BASE_URL,*/ "http://127.0.0.1:8000/api",
+      baseURL: config.API_BASE_URL /* "http://127.0.0.1:8000/api",*/,
       method: "POST",
       body: fd,
     });
