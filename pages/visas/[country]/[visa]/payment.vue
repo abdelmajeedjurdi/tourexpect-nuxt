277e5b1f26e4a9ae3dac6c8b8f43deb25402ebda
Edo Khudeeda <edo.khudeeda@gmail.com>
2023-04-20 15:05:35 +0300

    dd

--- pages/visas/[country]/[visa]/payment.vue
+++ pages/visas/[country]/[visa]/payment.vue
@@ -284,7 +284,7 @@ const payUsingFastpay = async () => {
           store_id: "874446_785",
           store_password: "Hevar@765176",
           order_id: application_forms.value[0]["visa"], //uniqueId,
-          bill_amount: 250, //total_to_dollar,
+          bill_amount: total_to_dollar,
           currency: "IQD",
           cart: `[{"name":"UAE Visa for Iraqi Passport","qty":${application_forms.value.length},"unit_price":${unit_price_to_dollar},"sub_total":${total_to_dollar}}]`,
         },
